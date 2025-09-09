58f73a58be6ada49e0f31f89a81cb195c1995d7a
Velvet <velbemond@ens.etsmtl.ca>
2025-09-09 17:01:49 -0400

    Étape 4 finito

--- test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -1,9 +1,28 @@
 // Vous devez insérer les nouveaux tests ici
 import { assert } from 'console';
 import 'jest-extended';
+import supertest from 'supertest';
+import app from '../../src/app'
+import { jeuRoutes } from '../../src/routes/jeuRouter';
 
-describe('redemarrerJeu.test.ts', () => {
-  it("devrait implémenter test", async () => {
-    throw new Error("Ce test n'a pas été défini")
+const request = supertest(app);
+
+const testNom1 = "Jean"
+const testNom2 = "Luc"
+
+describe('GET /api/v1/jeu/redemarrerJeu', () => {
+  beforeAll(async () => {
+    await request.post('/api/v1/jeu/demarrerJeu/').send({ nom: testNom1 });
+    await request.post('/api/v1/jeu/demarrerJeu/').send({ nom: testNom2 });
+  })
+  it('devrait redémarrer le jeu et retourner le code HTTP 200', async() => {
+    const response = await request.get('/api/v1/jeu/redemarrerJeu/').send();
+    expect(response.status).toBe(200);
+    expect(response.type).toBe("application/json");
+  });
+  it('devrait avoir supprimer tous les joueurs', async() => {
+    const joueursJSON = jeuRoutes.controleurJeu.joueurs;
+    const joueursArray = JSON.parse(joueursJSON);
+    expect(joueursArray.length).toBe(0);
   });
 });
