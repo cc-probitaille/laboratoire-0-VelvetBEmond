40e4cc95eaa1a23c2f86898eaa3f2fd5c3716b2d
Velvet <velbemond@ens.etsmtl.ca>
2025-09-16 15:13:33 -0400

    Étape 8

--- test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
+++ test/routes/jeuRouter-redemarrerJeu-lab0.test.ts
@@ -10,13 +10,13 @@ const request = supertest(app);
 const testNom1 = "Jean"
 const testNom2 = "Luc"
 
-describe('GET /api/v1/jeu/redemarrerJeu', () => {
+describe('get(/api/v1/jeu/redemarrerJeu)', () => {
   beforeAll(async () => {
     await request.post('/api/v1/jeu/demarrerJeu/').send({ nom: testNom1 });
     await request.post('/api/v1/jeu/demarrerJeu/').send({ nom: testNom2 });
   })
   it('devrait redémarrer le jeu et retourner le code HTTP 200', async() => {
-    const response = await request.get('/api/v1/jeu/redemarrerJeu/').send();
+    const response = await request.get('/api/v1/jeu/redemarrerJeu').send();
     expect(response.status).toBe(200);
     expect(response.type).toBe("application/json");
   });
@@ -25,4 +25,8 @@ describe('GET /api/v1/jeu/redemarrerJeu', () => {
     const joueursArray = JSON.parse(joueursJSON);
     expect(joueursArray.length).toBe(0);
   });
+  it('ne devrait plus être jouable sauf si on recrée un autre joueur', async() => {
+    const response = await request.get('/api/v1/jeu/jouer/' + testNom1);
+    expect(response.status).toBe(404);
+  })
 });
